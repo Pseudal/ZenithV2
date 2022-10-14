fbb30109cb0dadd5ed38f594349224333a0400ea
BenjaminL925 <b-lecaillet@hotmail.com>
2022-10-14 17:10:11 +0200

    A MODIFIER
    ajouter lien balise link  emotion.jsx
    ligne27

--- assets/components/homepage/_emotion.jsx
+++ assets/components/homepage/_emotion.jsx
@@ -22,7 +22,7 @@ function Emotion() {
             		</div>
 								</div>
           		<h4 className="titreB light letterSpacing container center accroche" data-aos="fade-up" data-aos-duration="1000">SUSCITER L’EMOTION EN UN INSTANT</h4>
-          		<button className="marginBottom2 btnS center light letterSpacingM" data-aos="fade-up" data-aos-duration="1000">EN VOIR +</button>
+    <link></link>      		<button className="marginBottom2 btnS center light letterSpacingM" data-aos="fade-up" data-aos-duration="1000">EN VOIR +</button>
 							</div>
         		</div>
       		</div>
