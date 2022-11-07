a1222a72d790e9791bc847ab44bf489f87358440
Pseudal <adrien.g@gmx.fr>
2022-11-07 23:02:37 +0100

    navup

--- assets/components/_navbar.jsx
+++ assets/components/_navbar.jsx
@@ -28,11 +28,11 @@ function navbar() {
       </label>
     
       <ul className="menu__box" data-aos="fade-right">
-        <form onSubmit={teleport}><input className="inputMail menu__item1 light letterSpacingS" type="text" id="email" size="10" required placeholder="Chercher" value={searchInput} onChange={handleChange}></input></form>
+        <form onSubmit={teleport}><input className="inputMail menu__item1 light letterSpacingS" type="text" id="email" size="8" required placeholder="Chercher" value={searchInput} onChange={handleChange}></input></form>
         <h5 className="menu__item light letterSpacingS"><Link className="textDeco marginStop" to="/">ACCUEIL</Link></h5>
         <h5 className="menu__item light letterSpacingS"><Link className="textDeco" to="/projets">PROJETS</Link></h5>
         <h5 className="menu__item light letterSpacingS"><Link className="textDeco" to="/contact">CONTACT</Link></h5>
-        <h5 className="menu__item light letterSpacingS"><Link className="textDeco" to="/client">CLIENTS</Link></h5>
+        <h5 className="menu__item light letterSpacingS"><Link className="textDeco" to="/clients">CLIENTS</Link></h5>
         <a className="textDeco letterSpacingS" href="/login"><h5 className="menu__item textDeco light">ADMIN</h5></a>
       </ul>
 		</>
