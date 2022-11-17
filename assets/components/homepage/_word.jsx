1572d3c6fdba5a2827c4b1b5cfa5784a6810ab18
BenjaminL925 <b-lecaillet@hotmail.com>
2022-11-17 14:08:46 +0100

    integration ligne qui bouge
    2fonctions

--- assets/components/homepage/_word.jsx
+++ assets/components/homepage/_word.jsx
@@ -1,20 +1,41 @@
 import React, { useState } from "react"
 import "swiper/css";
 
+const wordpage = ({ text}) => {
 
-
-
-function Wordpage() {
-
-		const [isOpen, setIsOpen] = useState(false);
-
-		function toggleModal() {
-			setIsOpen(!isOpen);
+	const [key1,key2]= useState(1);
+	
+	const scrollingxY=  useSpring({
+	from: {transform : ("translate (60%,0")         },
+	to:{ transform : ("translate (60%,0")        },
+	config: {duration :2000},
+	reset: true,
+	
+	onRest: (   )=> {
+	key1(	key2 + 1)
+	}
+	
+	
+	})
+	
+	//other fonction pour mouvement ligne inverse
+	
+	const [text,mvtext]=useState(1)
+	
+	const scrollingY=  useSpring({
+		from: {transform : ("translate (-60%,0")         },
+		to:{ transform : ("translate (-60%,0")        },
+		config: {duration :2000},
+		reset: true,
+		
+		onRest: (   )=> {
+		test( mvtext + 1)
 		}
+	
+	})
+	
 
-		function closeModal() {
-			setIsOpen(false);
-		}
+
 
 
 
@@ -32,27 +53,27 @@ function Wordpage() {
 
         	<h1 className="wordpage light  grid1" id='wordpage'>RÉACTIVITÉ</h1>
 					<h1 className=" margin medium wordpageText grid2">·</h1>
-        	<h1 onClick={toggleModal} className="wordpage outline normal grid3">PROXIMITÉ</h1>
+        	<h1 onMouseOver={scrollingxY} className="wordpage outline normal grid3">PROXIMITÉ</h1>
 					<h1 className=" margin medium wordpageText grid4">·</h1>
-        	<h1 onClick={toggleModal} className="wordpage light  grid5">PASSION</h1>
+        	<h1 onMouseOver={scrollingxY} className="wordpage light  grid5">PASSION</h1>
 					<h1 className=" wordpageText  medium grid6">·</h1>
-        	<h1 onClick={toggleModal} className="wordpage normal  grid7">STRATÉGIE</h1>
+        	<h1 onMouseOver={scrollingxY} className="wordpage normal  grid7">STRATÉGIE</h1>
 
-          <h1 onClick={toggleModal} className="wordpage normal  grid9">COMMUNICATION</h1>
+          <h1 onMouseOver={scrollingY} className="wordpage normal  grid9">COMMUNICATION</h1>
 					<h1 className=" wordpageText outline medium grid10">·</h1>
-        	<h1 onClick={toggleModal} className="wordpage marron normal  grid11">ZENITH</h1>
+        	<h1 onMouseOver={scrollingY} className="wordpage marron normal  grid11">ZENITH</h1>
 					<h1 className=" wordpageText outline medium grid12">·</h1>
-        	<h1 onClick={toggleModal} className="wordpage normal  grid13">CONSEIL</h1>
+        	<h1 onMouseOver={scrollingY} className="wordpage normal  grid13">CONSEIL</h1>
 					<h1 className=" wordpageText medium grid14">·</h1>
-        	<h1 onClick={toggleModal} className="wordpage normal  grid15">EVENT</h1>
+        	<h1 onMouseOver={scrollingY} className="wordpage normal  grid15">EVENT</h1>
 					
-        	<h1 onClick={toggleModal} className="wordpage outline medium  grid17">CRÉATIVITÉ</h1>
+        	<h1 onMouseOver={scrollingxY} className="wordpage outline medium  grid17">CRÉATIVITÉ</h1>
 					<h1 className=" wordpageText medium grid18">·</h1>
-        	<h1 onClick={toggleModal} className="wordpage light  grid19">RÊVER</h1>
+        	<h1 onMouseOver={scrollingxY} className="wordpage light  grid19">RÊVER</h1>
 					<h1 className=" wordpageText medium grid20">·</h1>
-        	<h1 onClick={toggleModal} className="wordpage light  grid21">AMBITION</h1>
+        	<h1 onMouseOver={scrollingxY} className="wordpage light  grid21">AMBITION</h1>
 					<h1 className=" wordpageText medium grid22">·</h1>
-        	<h1 onClick={toggleModal} className="wordpage outline normal  grid23">IMAGINATION</h1> 
+        	<h1 onMouseOver={scrollingxY} className="wordpage outline normal  grid23">IMAGINATION</h1> 
 				</div>
       	</div> 
 
