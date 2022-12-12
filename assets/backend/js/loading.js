fa28c4e42091da0c8350018deaad5dc68419e430
Pseudal <adrien.g@gmx.fr>
2022-12-12 19:36:46 +0100

    for merge

--- assets/backend/js/loading.js
+++
@@ -1,6 +0,0 @@
-var readyStateCheckInterval = setInterval(function() {
-    if (document.readyState === "complete") {
-        clearInterval(readyStateCheckInterval);
-        $("#loading").fadeOut();
-    }
-}, 10);
